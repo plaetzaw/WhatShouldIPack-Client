c4a44364fff5f01482c47128a33492213e7d7f3c
plaetzaw <plaetzaw@gmail.com>
2021-02-12 17:24:19 -0600

    Basic components built on page, ready to send information to server

---
+++ components/clothingcard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'primereact/card'
+
+const ClothingCard = ({ props }) => {
+  return (
+    <>
+      <Card>
+        I'm going to show clothing
+      </Card>
+    </>
+  )
+}
+
+export default ClothingCard
